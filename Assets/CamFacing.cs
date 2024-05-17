4ecfd490b5f0b3ea46e55340e58e9aea13b5a1c4
bennieb1 <brandon961@live.ca>
2024-05-17 14:06:03 -0400

    123

---
+++ Assets/CamFacing.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFacing : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform.position);
+        transform.Rotate(new Vector3(0, 180, 0));
+    }
+}
