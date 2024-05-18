85c3dc87706bc1535bd50eba8c46137b50239741
bennieb1 <brandon961@live.ca>
2024-05-18 18:24:28 -0400

    brought in scene

---
+++ Assets/Game/Scripts/Recievers.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recievers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void RightFoot() {
+    
+        Debug.Log("Right step made");
+
+    }
+
+    public void LeftFoot()
+    {
+    
+        Debug.Log("Left step made");
+
+    }
+}
