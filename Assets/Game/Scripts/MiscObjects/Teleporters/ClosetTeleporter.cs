91f7e7d7976b3f3856e7435cf913511a900bd41b
sami-s-56 <samisyed301@gmail.com>
2024-05-28 16:29:36 -0400

    Work On Teleporter

---
+++ Assets/Game/Scripts/MiscObjects/Teleporters/ClosetTeleporter.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosetTeleporter : Teleporter
+{
+    [SerializeField] Light closetLight;
+    [SerializeField] float lightToggleDelay = .5f;
+
+    float curTime = 0f;
+    bool bInteracting = false;
+
+    protected override void Interact()
+    {
+        //To be changed to Input Event
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            bInteracting = true;
+        }
+
+        if (bInteracting)
+        {
+            curTime += Time.deltaTime;
+            if(curTime > lightToggleDelay) 
+            {
+                closetLight.enabled = false;
+                Teleport();
+                ResetTeleporter();
+            }
+        }
+    }
+
+    private void ResetTeleporter()
+    {
+        closetLight.enabled = true;
+        curTime = 0f;
+        bInteracting = false;
+    }
+}
