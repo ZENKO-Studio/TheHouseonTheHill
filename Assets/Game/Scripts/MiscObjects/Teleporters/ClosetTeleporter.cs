c787c11c113eb2e72b5c9d52ad87ba00a46a6cc5
sami-s-56 <samisyed301@gmail.com>
2024-05-29 12:56:01 -0400

    Some Updates made to teleporter

--- Assets/Game/Scripts/MiscObjects/Teleporters/ClosetTeleporter.cs
+++ Assets/Game/Scripts/MiscObjects/Teleporters/ClosetTeleporter.cs
@@ -25,6 +25,9 @@ public class ClosetTeleporter : Teleporter
             if(curTime > lightToggleDelay) 
             {
                 closetLight.enabled = false;
+            }
+            if(curTime > lightToggleDelay*2f)
+            {
                 Teleport();
                 ResetTeleporter();
             }
