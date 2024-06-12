5d35d98824f6c3615ab252afdb06ec808732f796
bennieb1 <brandon961@live.ca>
2024-06-12 19:34:18 -0400

    built my house

--- Assets/Game/Scripts/Interactable/DummyInteractableKey.cs
+++ Assets/Game/Scripts/Interactable/DummyInteractableKey.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DummyInteractableKey : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private string requiredKey;
+    private bool _isUnlocked;
+
+    public UnityEvent onUnlock;
+
+    public void Unlock(string key)
     {
-        
+        if (key == requiredKey)
+        {
+            _isUnlocked = true;
+            onUnlock?.Invoke();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public  void Interact(CharacterBase player)
     {
-        
+        if (_isUnlocked)
+        {
+            Interact(player);
+        }
+        else
+        {
+            Debug.Log("The object is locked. You need the correct key to unlock it.");
+        }
     }
 }
+
