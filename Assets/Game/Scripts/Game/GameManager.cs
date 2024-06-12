7e0eeab060e2639f8a98119119963d7e9be8e41b
sami-s-56 <samisyed301@gmail.com>
2024-06-12 11:23:58 -0400

    Updates

--- Assets/Game/Scripts/Game/GameManager.cs
+++ Assets/Game/Scripts/Game/GameManager.cs
@@ -1,56 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+/**
+ * @Sami 06/12/24
+ * #TODO Prolly remove this as I have used GameHandler in multiple places since long time ago (feeling bored to replace all the references)
+ **/
 
-public class GameManager : MonoBehaviour
-{
-    public static GameManager Instance { get; private set; }
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-    public Inventory inventory;
-    public GameObject keyItemPrefab;
-    public GameObject resourceItemPrefab;
-    public NellAttributes playerRef;
+//public class GameManager : MonoBehaviour
+//{
 
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject); // Make this object persist across scene loads
-        }
-        else
-        {
-            Destroy(gameObject); // Destroy duplicate instances
-            return;
-        }
-    }
+//    public static GameManager Instance { get; private set; }
 
-    private void Start()
-    {
-        // Add some items to the inventory for testing
-        AddKeyItem("Golden Key");
-        // AddResourceItem("Salt", 10);
-    }
+//    public Inventory inventory;
+//    public GameObject keyItemPrefab;
+//    public GameObject resourceItemPrefab;
+    
+//    private void Awake()
+//    {
+//        if (Instance == null)
+//        {
+//            Instance = this;
+//            DontDestroyOnLoad(gameObject); // Make this object persist across scene loads
+//        }
+//        else
+//        {
+//            Destroy(gameObject); // Destroy duplicate instances
+//            return;
+//        }
+//    }
+//    private void Start()
+//    {
+//        // Add some items to the inventory for testing
+//        AddKeyItem("Golden Key");
+//        //AddResourceItem("Salt", 10);
+//    }
 
-    public void AddKeyItem(string name)
-    {
-        GameObject keyItemObject = Instantiate(keyItemPrefab);
-        KeyItem keyItem = keyItemObject.GetComponent<KeyItem>();
-        keyItem.itemName = name;
-        inventory.AddItem(keyItem);
-    }
+//    public void AddKeyItem(string name)
+//    {
+//        GameObject keyItemObject = Instantiate(keyItemPrefab);
+//        KeyItem keyItem = keyItemObject.GetComponent<KeyItem>();
+//        keyItem.itemName = name;
+//        inventory.AddItem(keyItem);
+//    }
 
-    public void AddResourceItem(string name, int amount)
-    {
-        GameObject resourceItemObject = Instantiate(resourceItemPrefab);
-        Resource resourceItem = resourceItemObject.GetComponent<Resource>();
-        resourceItem.itemName = name;
-        resourceItem.amount = amount;
-        inventory.AddItem(resourceItem);
-    }
-
-    public void ToggleInventory(bool isOpen)
-    {
-        EventBus.Publish(new EventBus.ToggleInventoryEvent(isOpen));
-    }
-}
+//    public void AddResourceItem(string name, int amount)
+//    {
+//        GameObject resourceItemObject = Instantiate(resourceItemPrefab);
+//        Resource resourceItem = resourceItemObject.GetComponent<Resource>();
+//        resourceItem.itemName = name;
+//        resourceItem.amount = amount;
+//        inventory.AddItem(resourceItem);
+//    }
+//    public void ToggleInventory(bool isOpen)
+//    {
+//        EventBus.Publish(new EventBus.ToggleInventoryEvent(isOpen));
+//    }
+//}
--- Assets/Game/Scripts/UI/HUDController.cs
+++ Assets/Game/Scripts/UI/HUDController.cs
@@ -12,15 +12,11 @@ public class HUDController : MonoBehaviour
     // Start is called before the first frame update
     void OnEnable()
     {
-<<<<<<< Updated upstream
-        if (GameManager.Instance.playerRef != null)
-            GameManager.Instance.playerRef.OnHealthChanged.AddListener(UpdateHealthbar);
-=======
+
         hudMenu = GetComponent<HUDMenu>();
 
         if (GameHandler.Instance.playerRef != null)
             GameHandler.Instance.playerRef.OnHealthChanged.AddListener(UpdateHealthbar);
->>>>>>> Stashed changes
     }
 
     // Update is called once per frame
@@ -31,18 +27,14 @@ public class HUDController : MonoBehaviour
 
     void UpdateHealthbar()
     {
-<<<<<<< Updated upstream
-        healthBar.value = GameManager.Instance.playerRef.GetHealth();
-=======
         healthBar.value = GameHandler.Instance.playerRef.GetHealth();
         hudMenu.ShowHUD();
         Invoke(nameof(hudMenu.HideHUD), 5f);
->>>>>>> Stashed changes
     }
 
     void OnDisable()
     {
-        if (GameManager.Instance.playerRef != null)
-            GameManager.Instance.playerRef.OnHealthChanged.RemoveListener(UpdateHealthbar);
+        if (GameHandler.Instance.playerRef != null)
+            GameHandler.Instance.playerRef.OnHealthChanged.RemoveListener(UpdateHealthbar);
     }
 }
