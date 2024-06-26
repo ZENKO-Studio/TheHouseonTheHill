74316407063290066cb2cc491fbff197f0e653ed
sami-s-56 <samisyed301@gmail.com>
2024-06-26 13:13:58 -0400

    Game Manager Updated, Unused UI Scene Removed

--- Assets/Game/Scripts/Game/GameManager.cs
+++ Assets/Game/Scripts/Game/GameManager.cs
@@ -2,13 +2,31 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    //Should be set on game start or manually in the scene
-    public NellController playerRef;
+    #region Core Gameplay Prefabs References
 
-    //Should be set when camera switches (Set it to null when using Third Person Camera)
+    [Tooltip("Reference to the Nell`s Prefab")]
+    [SerializeField] GameObject NellPrefab;
+    
+    [Tooltip("Reference to the Inventory System Prefab")]
+    [SerializeField] GameObject InventorySystemPrefab;
+    
+    [Tooltip("Reference to the HUD prefab")]
+    [SerializeField] GameObject HUDPrefab;
+
+    #endregion
+
+
+    //Should be set on game start or manually in the scene
+    [HideInInspector] public NellController playerRef;
+
+    #region Cam View and Player Movement Orientation
+    //Can be used for movable objects since it requires some transform to base direction off
+    [Tooltip("Should be set when camera switches (Set it to null when using Third Person Camera)")]
     Transform activeCamTransform = null;
     
     public bool bUsingStaticCam;
+    
+    #endregion
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +38,48 @@ public class GameManager : Singleton<GameManager>
     void Update()
     {
         
+    }
+
+    //To be called when game scene is loaded
+    void SetupCoreComponents()
+    {
+        #region Ensure all the core components are setup
+        if (playerRef == null)
+        {
+            Debug.LogError($"Please Setup Nell Prefab in {name}"); 
+            return;
+        }
+        
+        if (HUDPrefab == null)
+        {
+            Debug.LogError($"Please Setup HUD Prefab in {name}"); 
+            return;
+        }
+
+        if (InventorySystemPrefab == null)
+        {
+            Debug.LogError($"Please Setup Inventory System Prefab in {name}");
+            return;
+        }
+        #endregion
+
+        #region Spawn all the stuff
+
+        //Player 
+        //Get this from check point system
+        Transform checkpointTrans = null;
+
+        playerRef = Instantiate(NellPrefab).GetComponent<NellController>();
+        playerRef.transform.position = checkpointTrans.position;
+        playerRef.transform.rotation = checkpointTrans.rotation;
+
+        //HUD
+        Instantiate(HUDPrefab);
+
+        //Inventory System
+        Instantiate(InventorySystemPrefab);
+
+        #endregion
     }
 
     //Get Active Camera
