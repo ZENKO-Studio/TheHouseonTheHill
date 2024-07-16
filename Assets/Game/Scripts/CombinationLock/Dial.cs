b42bf5d6b1cb5585a7e64028d46fcf4543b2857c
bennieb1 <brandon961@live.ca>
2024-07-16 15:34:04 -0400

    123

--- Assets/Game/Scripts/CombinationLock/Dial.cs
+++ Assets/Game/Scripts/CombinationLock/Dial.cs
@@ -8,12 +8,13 @@ using Random = UnityEngine.Random;
 public class Dial : MonoBehaviour
 {
     public float rotationSpeed = 10f;
-    public float rayDistance;
+    public float rayDistance = 100f;
     public LayerMask layer;
+    public string targetTag = "RotatableDial"; // Tag to identify specific dials
 
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
             RotateDial();
         }
@@ -21,19 +22,20 @@ public class Dial : MonoBehaviour
 
     private void RotateDial()
     {
-        Ray ray = new Ray(transform.position, transform.forward);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
         if (Physics.Raycast(ray, out hit, rayDistance, layer))
         {
-            if (!hit.collider.CompareTag("RotatableDial")) return;
-            
-            // If the ray hits an object, log the name of the object
-            Debug.Log("Hit object: " + hit.collider.gameObject.name);
-            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
-            // You can also access other information about the hit object, like its position
-            Debug.Log("Hit position: " + hit.point);
-
+            // Check if the hit object has the specified tag
+            if (hit.collider.gameObject.CompareTag(targetTag))
+            {
+                // If the ray hits an object with the correct tag, log the name of the object
+                Debug.Log("Hit object: " + hit.collider.gameObject.name);
+                hit.collider.gameObject.transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+                // You can also access other information about the hit object, like its position
+                Debug.Log("Hit position: " + hit.point);
+            }
         }
     }
 
