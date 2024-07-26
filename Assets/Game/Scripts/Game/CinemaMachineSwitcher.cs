442898ff16bc5130ddec975778736c6150ba75fa
sami-s-56 <samisyed301@gmail.com>
2024-07-26 17:30:05 -0400

    Camera Transition

--- Assets/Game/Scripts/Game/CinemaMachineSwitcher.cs
+++ Assets/Game/Scripts/Game/CinemaMachineSwitcher.cs
@@ -11,6 +11,9 @@ public class CinemaMachineSwitcher : MonoBehaviour
 
     [Tooltip("If this is set to true it will automatically switch orientation even if player is moving")]
     [SerializeField] bool instantlySwitchOrientation = false; 
+    
+    [Tooltip("Uncheck this if you want the transition animation to not play on this very trigger")]
+    [SerializeField] bool playTransitionAnimation = true; 
 
     private void OnTriggerEnter(Collider other)
     {
@@ -22,10 +25,22 @@ public class CinemaMachineSwitcher : MonoBehaviour
 
     private void OnEnter()
     {
+        if(playTransitionAnimation)
+        {
+            GameManager.Instance.playerHud.PlayBlinkAnim(this);
+        }
+        else
+        {
+            Blink();
+        }
+    }
+
+    //Called when eyes closed from animation
+    public void Blink()
+    {
         onEnter.Invoke();
         GameManager.Instance.playerRef.UpdateOrientation(instantlySwitchOrientation);
     }
-
 }
 
 
