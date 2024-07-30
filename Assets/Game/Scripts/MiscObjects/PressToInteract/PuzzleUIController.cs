2f3ae4c50463ac6febb9a493e45ebc4d2c7d2cc7
sami-s-56 <samisyed301@gmail.com>
2024-07-30 17:31:06 -0400

    Updates on Jigsaw

---
+++ Assets/Game/Scripts/MiscObjects/PressToInteract/PuzzleUIController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleUIController : MonoBehaviour
+{
+    internal static PuzzleUIItem itemBeingDragged;
+
+    internal void Validate()
+    {
+        throw new NotImplementedException();
+    }
+}
