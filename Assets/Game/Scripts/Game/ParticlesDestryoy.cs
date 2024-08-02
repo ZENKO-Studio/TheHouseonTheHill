4289859eff796f7bc2213133c3742f9a60bf94f7
bennieb1 <brandon961@live.ca>
2024-08-02 15:14:42 -0400

    can play some of it

--- Assets/Game/Scripts/Game/ParticlesDestryoy.cs
+++ Assets/Game/Scripts/Game/ParticlesDestryoy.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ParticlesDestryoy : MonoBehaviour
 {
+    public UnityEvent onInteract; 
     private void OnParticleCollision(GameObject other)
     {
         // Add any additional checks here, such as verifying the type of particle system
         Destroy(gameObject);
+        onInteract?.Invoke();
+       
     }
 }
