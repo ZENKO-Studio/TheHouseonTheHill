f2d0e92cd387d772bfdc3e7936488cdf5b41ddb7
sami-s-56 <85152176+sami-s-56@users.noreply.github.com>
2024-08-09 13:22:42 -0400

    Sami before messup (#199)
    * Update NellController.cs
    
    Fixed the bug of Stamina not generating when shift was pressed and player standing
    
    * The Pic Taking thing done
    
    * Update NellController.cs
    
    Resolving the previous conflict
    
    * Board Drag and Drop Working and hooked up to Inventory
    
    * fixing issues
    
    * Game Manager Updated, Unused UI Scene Removed
    
    * Nell Can now jump!!
    
    * Refactoring Inv Sys
    
    * Refactored the Inventory System (Tested and Working)
    
    * Updates on The Board
    
    * Salt Thing
    
    * Salt System Addition
    
    * Board Updates
    
    * The Board System Completed (Working with Inventory)
    
    * Salt thing hooked up to HUD, Player Jump Crouch Bug Solved
    
    * Removed Logs and Setup Particles on Nells Prefab
    
    * Applied final values to Inventory Item prefabs
    
    * Core + UI Setup
    
    * Slot Color and Validate Fix
    
    * Updates with UI  Scene Setup
    
    * Work on movable objects
    
    * Updates on Moving Table
    
    * Movable Objects Complete
    
    * Added Animations to the stalker
    
    * Part of Stalker animations
    
    * Animation Sequences
    
    * Updates on Animator Trigger and Movable Script
    
    * Movable Object Bug Fixed
    
    * Blood fx
    
    * Vfx Updates
    
    * W to Push
    
    * Player orientation thing updated
    
    * Update NellController.cs
    
    Minor Camera Fix
    
    * Glow for stalker stun
    
    * Stalker Updated
    
    * Glowing Inventory Items
    
    * Moving Table stuff done
    
    * Salt mechanic changed
    
    * Added Hold Interactables and HUD Script Updated
    
    * Salt Pickup Added
    
    * Update
    
    Nell Updated
    Swimming Added
    Inventory Item shows up on HUD when added to inventory
    Something else?
    
    * Update NellController.cs
    
    Uncommented the code that was commented and causing issue
    
    * Updates
    
    * Camera Transition
    
    * Blink Effect Transitions added
    
    * Update CinemaMachineSwitcher.cs
    
    * Updates on Jigsaw
    
    * Jigsaw working, Unused animations deleted
    
    * player crouching thing fixed
    
    * Work on Improving Menu System
    
    * Level02(Added assets, changed lighting, Changed the layout for stealth section)
    
    * Update SaltChargeHandler.cs
    
    * Menu glitch fixed
    
    * Added the props and assets to the level and the final puzzle section
    
    * Generator working with dialogue and inventory
    
    * Journal Entry Prefabs Created!
    
    * Added few props and created a cutscene.
    
    * Fixed Generator error Code
    
    * ControllerUpdates
    
    * Update NellController.cs
    
    Character Movement thing finally fixed
    
    * Added Journal Entries collectables, props, Eleanor photo to the Level
    
    * Level 02  (#195)
    
    * Level02(Added assets, changed lighting, Changed the layout for stealth section)
    
    * Added the props and assets to the level and the final puzzle section
    
    * Added few props and created a cutscene.
    
    * Fixed Generator error Code
    
    * Restoring Deleted things
    
    ---------
    
    Co-authored-by: Brandon Bennie <97978644+bennieb1@users.noreply.github.com>
    Co-authored-by: Rohith2510 <rohithr2510@gmail.com>
    Co-authored-by: Rohith2510 <157414144+Rohith2510@users.noreply.github.com>
    Co-authored-by: bennieb1 <brandon961@live.ca>

--- Assets/Game/Scripts/MiscObjects/HoldToInteractObjects/Generator.cs
+++ Assets/Game/Scripts/MiscObjects/HoldToInteractObjects/Generator.cs
@@ -4,7 +4,32 @@ using UnityEngine;
 
 public class Generator : HoldInteractable
 {
+    enum GeneratorState
+    {
+        Start,
+        Identified,
+        Unusable,
+        Usable,
+        Used
+    }
+
+    GeneratorState curState = GeneratorState.Start;
+
+    [Header("Dialogue Setup")]
+
+    [Tooltip("Dialogue that will be played once when player enters the trigger for the first time")]
+    [SerializeField] List<string> initialLines = new List<string>();
+    
+    [Tooltip("Dialogue that will be played once when player enters the trigger and do not have required items")]
+    [SerializeField] List<string> linesWhenUnusable = new List<string>();
+    
+    [Tooltip("Dialogue that will be played once when player enters the trigger have all the items")]
+    [SerializeField] List<string> linesWhenUsable = new List<string>();
+
     [SerializeField] List<GameObject> lights;
+
+    [Tooltip("Thsese ids will be checked to ensure item is present (Item Type Usables")] 
+    [SerializeField] List<int> requiredItemIds = new List<int>();
 
     [SerializeField] int soundRange = 10;
 
@@ -21,8 +46,54 @@ public class Generator : HoldInteractable
         }
     }
 
+    protected override void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+
+        if (curState == GeneratorState.Start)
+        {
+            GameManager.Instance.playerHud.UpdateDialogueText(initialLines[Random.Range(0, initialLines.Count)], 3);
+            curState = GeneratorState.Identified;
+        }
+        else
+        {
+            CheckIfUsable();
+
+            if(curState == GeneratorState.Unusable)
+                GameManager.Instance.playerHud.UpdateDialogueText(linesWhenUnusable[Random.Range(0, linesWhenUnusable.Count)], 3);
+            else if(curState == GeneratorState.Usable)
+                GameManager.Instance.playerHud.UpdateDialogueText(linesWhenUsable[Random.Range(0, linesWhenUnusable.Count)], 3);
+
+        }
+    }
+
+    private void CheckIfUsable()
+    {
+        if (requiredItemIds.Count > 0)
+        {
+            foreach (int itemId in requiredItemIds)
+            {
+                if (!InventoryHandler.Instance.HasUsableItem(itemId))
+                    return;
+            }
+
+            curState = GeneratorState.Usable;
+
+        }
+    }
+
     protected override void OnTriggerStay(Collider other)
     {
+        if (curState == GeneratorState.Unusable || !other.CompareTag("Player"))
+            return;
+
+        if (playerRef.bInteracting && curState == GeneratorState.Identified)
+        {
+            GameManager.Instance.playerHud.UpdateDialogueText(linesWhenUnusable[Random.Range(0, linesWhenUnusable.Count)], 3);
+            curState = GeneratorState.Unusable;
+            return;
+        }
+
         base.OnTriggerStay(other);
     
         //Every One Second Make Sound
