9fb4b5a432c0b5fef5e1c66149aec48e5c49bc93
sami-s-56 <samisyed301@gmail.com>
2024-08-12 16:27:57 -0400

    Stalker can make some noise!

--- Assets/Game/Scripts/AI/Stalker/StalkerInvestigate.cs
+++ Assets/Game/Scripts/AI/Stalker/StalkerInvestigate.cs
@@ -10,6 +10,13 @@ public class StalkerInvestigate : StalkerBaseState
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         agent.SetDestination(lastPlayerPos);
+
+        if (stalkerRef.stalkerAudio)
+        {
+            stalkerRef.stalkerAudio.clip = stalkerRef.investigatingSound;
+            stalkerRef.stalkerAudio.loop = true;
+            stalkerRef.stalkerAudio.Play();
+        }
     }
 
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -55,6 +62,14 @@ public class StalkerInvestigate : StalkerBaseState
         {
             //Have some timer?
             fsm.ChangeState(StalkerFSM.PatrolState);
+        }
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (stalkerRef.stalkerAudio)
+        {
+            stalkerRef.stalkerAudio.Stop();
         }
     }
 }
