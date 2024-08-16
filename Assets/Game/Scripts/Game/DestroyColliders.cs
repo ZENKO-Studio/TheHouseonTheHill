f111da9d8ccfdf201bc0f1ce29fd6ce5917d3bcf
Brandon Bennie <97978644+bennieb1@users.noreply.github.com>
2024-08-16 16:11:44 -0400

    Brandons final stand (#240)
    * My First Commit on Kas Comp
    
    * 123
    
    ---------
    
    Co-authored-by: Kas <69044220+kasiapeia@users.noreply.github.com>

---
+++ Assets/Game/Scripts/Game/DestroyColliders.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyColliders : MonoBehaviour
+{
+    // Assign this in the Unity Inspector
+    public Collider colliderToDestroy;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (colliderToDestroy != null)
+        {
+            Destroy(colliderToDestroy.gameObject);
+        }
+    }
+}
