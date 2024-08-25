1dd6bf46886918c24bcb52cc6c5249d19090b0ba
Brandon Bennie <97978644+bennieb1@users.noreply.github.com>
2024-08-25 09:32:41 -0400

    Home branch new (#266)
    * 123
    
    * 123
    
    * FRI 9:53
    
    * 123
    
    * 213
    
    * 123
    
    * temp
    
    * 123
    
    * 123
    
    * elevator play on first floor
    
    * cutscene
    
    * cutscene
    
    * cutscene done and colliders on rails
    
    * we got snow
    
    * its snowing!
    
    * we have voice lines and dialogue we also have a skip cutscene option
    
    * fix
    
    * 123
    
    * 123
    
    * 123
    
    * no more button scene change
    
    * kEYS SHOULD BE REMOVED FROM INVENTORY AFTER USE
    
    * before rebake
    
    * 123

--- Assets/Game/Scripts/Interactable/OneSidedInteract.cs
+++ Assets/Game/Scripts/Interactable/OneSidedInteract.cs
@@ -12,6 +12,27 @@ public class OneSidedInteract : MonoBehaviour, IInteractable
     [SerializeField] private int priority;
 
     public OpenThings openThings;
+    
+    
+    #region StoleFromSami
+
+    
+
+    
+    [Header("Audio and Dialogue")]
+    [SerializeField] bool PlayDoorSound = false;
+    [SerializeField] bool PlayDialogueLines = false;
+
+    [SerializeField] AudioClip lockedDoorSound; 
+
+    [SerializeField] AudioClip unlockedDoorSound; 
+
+    [Tooltip("Dialogue that will be played once when player enters the trigger and do not have required items")]
+    [SerializeField] List<string> linesWhenLocked = new List<string>();
+
+    [Tooltip("Dialogue that will be played once when player enters the trigger have all the items")]
+    [SerializeField] List<string> linesWhenUnlocked = new List<string>();
+    #endregion
 
     private void Start()
     {
@@ -30,12 +51,24 @@ public class OneSidedInteract : MonoBehaviour, IInteractable
         {
           //e  Gizmos.DrawRay(player.transform.position , openThings.transform.position);
             onInteract?.Invoke();
+            if (PlayDoorSound)
+                AudioSource.PlayClipAtPoint(unlockedDoorSound, transform.position);
+                    
+            if (PlayDialogueLines)
+                GameManager.Instance.playerHud.UpdateDialogueText(linesWhenUnlocked[Random.Range(0, linesWhenUnlocked.Count)], 2);
         }
         else
         {
-            Debug.Log("Player is not in front of the door.");
+            if (PlayDoorSound)
+                AudioSource.PlayClipAtPoint(lockedDoorSound, transform.position);
+
+            if (PlayDialogueLines)
+                GameManager.Instance.playerHud.UpdateDialogueText(linesWhenLocked[Random.Range(0, linesWhenLocked.Count)], 2);
+            else
+                Debug.Log("Player is not in front of the door.");
         }
     }
+    
 
     public int Priority => priority;
 }
