a0d327606474fe950ddad8d2dff90982aa56e95a
Brandon Bennie <97978644+bennieb1@users.noreply.github.com>
2024-08-25 12:33:22 -0400

    Testingmypatience (#267)
    * 123
    
    * 123
    
    * FRI 9:53
    
    * 123
    
    * 213
    
    * 123
    
    * temp
    
    * 123
    
    * 123
    
    * elevator play on first floor
    
    * cutscene
    
    * cutscene
    
    * cutscene done and colliders on rails
    
    * we got snow
    
    * its snowing!
    
    * we have voice lines and dialogue we also have a skip cutscene option
    
    * fix
    
    * 123
    
    * 123
    
    * 123
    
    * no more button scene change
    
    * kEYS SHOULD BE REMOVED FROM INVENTORY AFTER USE
    
    * before rebake
    
    * 123
    
    * i did it sami
    
    * 123

--- Assets/Game/Scripts/Game/TimeLineHandler.cs
+++ Assets/Game/Scripts/Game/TimeLineHandler.cs
@@ -2,29 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class TimeLineHandler : MonoBehaviour
 {
-    public PlayableDirector timelineDirector;
-    public int levelToStart;
+    public VideoPlayer videoPlayer;  // Reference to the VideoPlayer
+    public int levelToStart;  // Index of the level to load
 
     void Start()
     {
-        // Subscribe to the stopped event
-        timelineDirector.stopped += OnTimelineStopped;
+        // Subscribe to the VideoPlayer's loopPointReached event
+        videoPlayer.loopPointReached += OnVideoEnd;
     }
 
-    void OnTimelineStopped(PlayableDirector director)
+    void OnVideoEnd(VideoPlayer vp)
     {
-        if (director == timelineDirector)
-        {
-            GameManager.Instance.StartLevel(levelToStart);
-        }
+        // Change to the next scene when the video ends
+        GameManager.Instance.StartLevel(levelToStart);
     }
 
     void OnDestroy()
     {
-        // Unsubscribe when destroyed
-        timelineDirector.stopped -= OnTimelineStopped;
+        // Unsubscribe when the object is destroyed
+        videoPlayer.loopPointReached -= OnVideoEnd;
     }
 }
